6d87630092714d6eda062981117ba7ea684fbe15
Marta Niemiec <niemiec.marta@hotmail.com>
2019-08-18 23:52:12 +0200

    Property Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);
+  }
 
   ngOnInit() {
   }
