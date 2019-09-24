aa0d09ab2d8bd78acfee3c9502ddd455cb6b029e
Marta Niemiec <niemiec.marta@hotmail.com>
2019-09-24 14:06:42 +0200

    Redirecting, Wildcard Routes, Outsourcing the route Configuration

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -10,19 +10,10 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes= [
-  { path: '', component: HomeComponent},
-  { path: 'users', component: UsersComponent, children: [
-    { path: ':id/:name', component: UserComponent},
-  ]},
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent},
-    { path: ':id/edit', component: EditServerComponent}
-  ]},
 
-]
 
 @NgModule({
   declarations: [
@@ -32,12 +23,13 @@ const appRoutes: Routes= [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
