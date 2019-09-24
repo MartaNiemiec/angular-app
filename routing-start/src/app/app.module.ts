3ed1ee43d4e0504f7f91b527f54881170c9b92cf
Marta Niemiec <niemiec.marta@hotmail.com>
2019-09-24 11:18:11 +0200

    Passing and retrieving Query Parameters and Fragmants

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -16,7 +16,9 @@ const appRoutes: Routes= [
   { path: '', component: HomeComponent},
   { path: 'users', component: UsersComponent},
   { path: 'users/:id/:name', component: UserComponent},
-  { path: 'servers', component: ServersComponent}
+  { path: 'servers', component: ServersComponent},
+  { path: 'servers/:id', component: ServerComponent},
+  { path: 'servers/:id/edit', component: EditServerComponent}
 ]
 
 @NgModule({
